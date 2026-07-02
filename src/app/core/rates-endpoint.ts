8e2db625eb2fee620e7a1d4a6819ab04868983b5
dimitrmo <dmouratidis153@gmail.com>
2026-07-02 20:55:48 +0300

    Updates

--- src/app/core/rates-endpoint.ts
+++ src/app/core/rates-endpoint.ts
@@ -1,6 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
-export const RATES_ENDPOINT_DEFAULT = 'ws://localhost:20002/ws';
+export const RATES_ENDPOINT_DEFAULT = '/ws';
 
 export const RATES_ENDPOINT = new InjectionToken<string>('RATES_ENDPOINT', {
     factory: () => RATES_ENDPOINT_DEFAULT,
